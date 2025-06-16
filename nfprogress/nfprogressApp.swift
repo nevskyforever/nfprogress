a3c3f8f8aa3988cb18cd0d5d12397e165b69cdc7
nevskyforever <138755791+nevskyforever@users.noreply.github.com>
2025-06-17 00:30:07 +0400

    Improve menu bar entry autosave

--- nfprogress/nfprogressApp.swift
+++ nfprogress/nfprogressApp.swift
@@ -1,13 +1,20 @@
 import SwiftUI
 import SwiftData
 
+@MainActor
 @main
 struct nfprogressApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: [WritingProject.self, Entry.self])
+        .modelContainer(DataController.shared)
+
+        MenuBarExtra("NFProgress", systemImage: "text.cursor") {
+            MenuBarEntryView()
+        }
+        .menuBarExtraStyle(.window)
+        .modelContainer(DataController.shared)
     }
 }
 
