e7fcf790f3852fdb4498460ed47f9ac9a53d5dd0
nevskyforever <138755791+nevskyforever@users.noreply.github.com>
2025-06-27 04:48:50 +0400

    Fix identifier for deadline reminders

--- nfprogress/nfprogressApp.swift
+++ nfprogress/nfprogressApp.swift
@@ -31,6 +31,9 @@ struct nfprogressApp: App {
             }
         }
 #endif
+#if canImport(UserNotifications)
+        DeadlineReminderManager.scheduleReminders()
+#endif
     }
     /// Глобальные настройки приложения, доступные во всех сценах
     @StateObject var settings = AppSettings()
