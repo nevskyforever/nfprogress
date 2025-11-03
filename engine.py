0f5ccc982e9e74b3739642b2a03451576c2ea1e4
nevskyforever <nevskyforever@mail.ru>
2025-11-03 17:58:31 +0400

    Обновление 0.0.2
    Улучшена логика хранения - теперь проекты записываются ы оперативную память списком и внутри программы меняется именно список, после чего файл полностью перезаписывается.
    
    Чтение/запись данных о проектах выделены в отдельную функцию.
    
    Улучшено отображение меню, добавлены отступы.

--- engine.py
+++ engine.py
@@ -1,22 +1,34 @@
+def read_file(filename):
+    with open(filename, 'r', encoding='UTF-8') as f:
+        data = f.readlines()
+        if data:
+            projects_lines = [line.replace('\n', '') for line in data]
+            return projects_lines
+    return []  # обязательно возвращаем пустой список
+
+def write_file(filename, pr_out):
+    with open(filename, 'w', encoding='UTF-8') as f:
+        f.write(pr_out)
+
 def main_menu():
-    # Словарь функций
-    menu = {'1': view_project, '2': new_note}
+    ch = False
+    while ch == False:
+        # Словарь функций
+        menu = {'1': view_projects, '2': new_project}
 
-    # Вывод меню
-    print('nfprogress 0.0.1')
-    print('Что вы хотите сделать?')
-    print()
-    print('1 - просмотреть проекты')
-    print('2 - добавить запись')
-
-    # Выбор пункта
-    ch = input()
-    if ch in menu:
-        menu[ch]()
-    else:
-        print('Неверный выбор')
-        main_menu()
-
+        # Вывод меню
+        ch = input('nfprogress 0.0.2\n'
+              '\n'
+            'Что вы хотите сделать?\n'
+            '1 - просмотреть проекты\n'
+            '2 - добавить проект\n'
+            '\n'
+            'Выбор: ')
+        if ch in menu:
+            menu[ch]()
+        else:
+            print('Неверный выбор')
+            main_menu()
 
 def new_project():
     print('Новый проект')
@@ -31,21 +43,29 @@ def new_project():
     main_menu()
 
 def view_projects():
-    with open('projects.txt', '') as f:
-        data = f.readlines()
-        if data:
-            projects_lines = [line.replace('\n', '') for line in data]
-            for project in projects_lines:
-                pr_out = project.split(' ')
-                written = round(sum([int(i) for i in pr_out[2:]]) / int(pr_out[1]) * 100)
+    projects = read_file('projects.txt')
+    if projects:
+        for project in projects:
+            pr_out = project.split(' ')
+            if len(pr_out) >= 2:  # проверяем что есть хотя бы название и цель
+                if len(pr_out) > 2:  # если есть написанные символы
+                    written = round(sum([int(i) for i in pr_out[2:]]) / int(pr_out[1]) * 100)
+                else:  # если нет написанных символов
+                    written = 0
                 print(f'Название: {pr_out[0]}, цель: {pr_out[1]}, написано: {written}%')
-            choice = input('Что вы хотите сделать?')
-            menu = {'1': new_note, '2': main_menu}
+        choice = input('Что вы хотите сделать?\n'
+                       '1 - новая запись\n'
+                       '2 - выйти в меню\n'
+                       '\n'
+                       'Выбор: ')
+        menu = {'1': new_note, '2': main_menu}
+        if choice in menu:
             menu[choice]()
-        else:
-            print('Проектов пока нет')
-            menu = {'д': new_project, 'н': view_project}
-            ch = input('Создать первый проект? (д/н): ')
+    else:
+        print('Проектов пока нет')
+        menu = {'д': new_project, 'н': main_menu}
+        ch = input('Создать первый проект? (д/н): ')
+        if ch in menu:
             menu[ch]()
 
 def open_project():
@@ -54,6 +74,7 @@ def open_project():
 def delete_project():
     pass
 
-
 def new_note():
     pass
+
+main_menu()
