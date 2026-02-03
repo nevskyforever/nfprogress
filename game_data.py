3263994158b417334888a80bb315f5e5d2a51d39
nevskyforever <nevskyforever@mail.ru>
2026-02-03 22:28:17 +0400

    Обновление 1.3
    Создан базовый класс игровых предметов
    Создан подкласс для существующих предметов-функций
    Создан единый регистр предметов
    Переделаны имеющиеся предметы
    Добавлены новые зелья здоровья

--- game_data.py
+++ game_data.py
@@ -26,23 +26,13 @@ cf_coins = [0, 1.0, 1.0625, 1.125, 1.1875, 1.25, 1.3125, 1.375, 1.4375, 1.5, 1.5
 lvl_coins_bonus = [0, 250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500, 3750, 4000, 4250, 4500, 4750, 5000, 5250, 5500, 5750, 6000, 6250, 6500, 6750, 7000, 7250, 7500, 7750, 8000, 8250, 8500, 8750, 9000, 9250, 9500, 9750, 10000, 10250, 10500, 10750, 11000, 11250, 11500, 11750, 12000, 12250, 12500, 12750, 13000, 13250, 13500, 13750, 14000, 14250, 14500, 14750, 15000, 15250, 15500, 15750, 16000, 16250, 16500, 16750, 17000, 17250, 17500, 17750, 18000, 18250, 18500, 18750, 19000, 19250, 19500, 19750, 20000, 20250, 20500, 20750, 21000, 21250, 21500, 21750, 22000, 22250, 22500, 22750, 23000, 23250, 23500, 23750, 24000, 24250, 24500, 24750, 25000]
 
 
-def health_add(do, add):
+def health_add_func(do, price, add):
     gamer = game.load_game()
     if gamer is None:
         return 'Игровой режим не активирован'
     coins = gamer['coins']
     price = 25
-    if do == 'buy':
-        if coins < price:
-            return f'Недостаточно монет для покупки, нужно минимум {price} монет'
-        else:
-            gamer['coins'] -= price
-            if 'health_add' not in gamer['items']:
-                gamer['items']['health_add'] = 0
-            gamer['items']['health_add'] += 1
-            game.save_game(gamer)
-            return f'Куплено зелье восстановления! Осталось монет: {gamer["coins"]}'
-    elif do == 'use':
+    if do == 'use':
         items = gamer['items'].get('health_add', 0)
         if items == 0:
             return 'Зелья восстановления нет в инвентаре'
@@ -53,26 +43,13 @@ def health_add(do, add):
                 gamer['health'] = 100
             game.save_game(gamer)
             return '\nПРИМЕНЕНО ЗЕЛЬЕ ВОССТАНОВЛЕНИЯ'
-    elif do == '?':
-        return ('\nЗелье восстанавливает здоровье на 10 единиц, но не выше 100 единиц'
-                f'\nСтоимость {price} монет')
 
-def lottery_ticket(do, price):
+def lottery_ticket_func(do, price, add=None):
     gamer = game.load_game()
     if gamer is None:
         return 'Игровой режим не активирован'
     coins = gamer['coins']
-    if do == 'buy':
-        if coins < price:
-            return f'Недостаточно монет для покупки, нужно минимум {price} монет'
-        else:
-            gamer['coins'] -= price
-            if 'lottery_ticket' not in gamer['items']:
-                gamer['items']['lottery_ticket'] = 0
-            gamer['items']['lottery_ticket'] += 1
-            game.save_game(gamer)
-            return f'Куплен Лотерейный билет! Осталось монет: {gamer["coins"]}'
-    elif do == 'use':
+    if do == 'use':
         items = gamer['items'].get('lottery_ticket', 0)
         if items == 0:
             return 'Лотерейного билета нет в инвентаре'
@@ -92,38 +69,23 @@ def lottery_ticket(do, price):
                 if i in win:
                     cnt += 1
             if cnt == 0:
-                print('В этот раз не повезло :(')
+                return 'В этот раз не повезло :('
             if cnt == 1:
                 win_prize = price * 10
-                print(f'ВЫ ВЫИГРАЛИ {win_prize} МОНЕТ! Совпало 1 число из 3.')
+                return f'ВЫ ВЫИГРАЛИ {win_prize} МОНЕТ! Совпало 1 число из 3.'
             if cnt == 2:
                 win_prize = price * 100
-                print(f'ВЫ ВЫИГРАЛИ {win_prize} МОНЕТ! Совпало 2 числа из 3.')
+                return f'ВЫ ВЫИГРАЛИ {win_prize} МОНЕТ! Совпало 2 числа из 3.'
             if cnt == 3:
                 win_prize = price * 1000
-                print(f'ВЫ ВЫИГРАЛИ СУПЕРПРИЗ {win_prize} МОНЕТ! Совпало 3 число из 3.')
-            gamer['coins'] += win_prize
-            game.save_game(gamer)
-            return '\n ИСПОЛЬЗОВАН ЛОТЕРЕЙНЫЙ БИЛЕТ \n'
-    elif do == '?':
-        return ('\nЛотерейный билет состоят из трех цифр и можно выиграть 100 монет при совпадении'
-                '\n1 числа, 1000 монет при 2 и 10000, если совпало 3 числа :)'
-                f'\nСтоимость {price} монет')
+                return f'ВЫ ВЫИГРАЛИ СУПЕРПРИЗ {win_prize} МОНЕТ! Совпало 3 число из 3.'
+        gamer['coins'] += win_prize
+        game.save_game(gamer)
 
-def freeze(do, price):
+def freeze_func(do, price, add=None):
     gamer = game.load_game()
     coins = gamer['coins']
-    if do == 'buy':
-        if coins < price:
-            return f'Недостаточно монет для покупки, нужно минимум {price} монет'
-        else:
-            gamer['coins'] -= price
-            if 'freeze' not in gamer['items']:
-                gamer['items']['freeze'] = 0
-            gamer['items']['freeze'] += 1
-            game.save_game(gamer)
-            return f'Куплена Заморозка! Осталось монет: {gamer["coins"]}'
-    elif do == 'use':
+    if do == 'use':
         items = gamer['items'].get('freeze', 0)
         if items == 0:
             return 'Заморозок нет в инвентаре'
@@ -148,15 +110,47 @@ def freeze(do, price):
         return 'Заморозка позволяет пропустить один день в стрике'
 
 class Item:
-    def __init__(self, price, level=0):
+    def __init__(self, tag, price, level=0):
         self.price = price
         self.level = level
+        self.tag = tag
+    def buy(self):
+        gamer = game.load_game()
+        coins = gamer['coins']
+        amount = gamer['items'].get(self.tag, 0)
+        if coins < self.price:
+            return 'Недостаточно монет для покупки'
+        else:
+            coins -= self.price
+            amount += 1
+            game.save_game(gamer)
+            return 'Предмет куплен'
 
 
 class FuncItem(Item):
-    def __init__(self, func, do, price=0, level=0):
+    def __init__(self, func, price=0, add=None, level=0):
         super().__init__(price, level)
         self._func = func        # храним внешнюю функцию
 
-    def run(self, do):           # отдельный метод для вызова
-        return self._func(do)
+    def run(self, do='use', price, add):           # отдельный метод для вызова
+        return self._func(do, price, add)
+
+# Инициализация объектов
+
+freeze = FuncItem(freeze_func, price=100, level=2)
+lottery_ticket = FuncItem(lottery_ticket_func, price=10)
+health_potion_5 = FuncItem(health_add_func, price=10, add=5)
+health_potion_25 = FuncItem(health_add_func, price=50, add=25)
+health_potion_50 = FuncItem(health_add_func, price=100, add=50)
+health_recovery = FuncItem(health_add_func, price=200, add=100)
+
+
+# Реестр предметов
+ITEM_REGISTRY = {'Зелья':
+                           {'Зелья здоровья':
+                                {'Малое зелье здоровья': health_potion_5,
+                                 'Среднее зелье здоровья': health_potion_25,
+                                 'Большое зелье здоровья': health_potion_50,
+                                 'Зелье воскрешения': health_recovery,}},
+                 'Заморозка': freeze,
+                 'Лотерейный билет': lottery_ticket,}
