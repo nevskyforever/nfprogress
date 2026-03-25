e87d965feb82bef4510003ebb1b8f96a5077db76
nevskyforever <nevskyforever@mail.ru>
2026-03-25 22:20:23 +0400

    Обновление 3.3.9
    Исправление багов

--- engine.py
+++ engine.py
@@ -13,7 +13,7 @@ from docx import Document
 dev_mode = False
 
 # Версия приложения
-version = '3.3.8 p1'
+version = '3.3.9'
 
 # Определяем систему
 SYSTEM = platform.system()  # 'Windows', 'Darwin' (macOS), 'Linux'
