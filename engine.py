2ce061c6815ee4cba0047d2ae60696f612b1fa52
nevskyforever <nevskyforever@mail.ru>
2026-04-08 12:48:55 +0400

    Обновление 3.3.18
    Исправлен баг Бесконечного проекта - он больше не создается заново, если уже есть.

--- engine.py
+++ engine.py
@@ -14,7 +14,7 @@ from docx import Document
 dev_mode = False
 
 # Версия приложения
-version = '3.3.17'
+version = '3.3.18'
 
 # Определяем систему
 SYSTEM = platform.system()  # 'Windows', 'Darwin' (macOS), 'Linux'
