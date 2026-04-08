010f3e2898a51bbbe9e37d28131f14bc59a4a263
nevskyforever <nevskyforever@mail.ru>
2026-04-08 16:10:38 +0400

    Обновление 3.3.19
    Добавлено отображения общего кол-ва символов, написанного сегодня во всех проектах. Показывается прямо над списком проектов. Опционально, нужно включить\отключить в настройках.

--- engine.py
+++ engine.py
@@ -11,10 +11,10 @@ from collections import defaultdict
 from docx import Document
 
 # Режим разработчика
-dev_mode = True
+dev_mode = False
 
 # Версия приложения
-version = '3.3.18'
+version = '3.3.19'
 
 # Определяем систему
 SYSTEM = platform.system()  # 'Windows', 'Darwin' (macOS), 'Linux'
