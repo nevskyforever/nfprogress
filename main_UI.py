e5becee089697921c3938f20621056d00ceaa203
nevskyforever <nevskyforever@mail.ru>
2026-04-12 22:03:42 +0400

    Теперь у Бесконечного проекта виджет проекта всегда отображает 100%
    В информации о проекте скрыта информация о прогрессе.

--- main_UI.py
+++ main_UI.py
@@ -377,11 +377,13 @@ class MainWindow(QMainWindow, main_window_ui):
         }
 
         # Обрабатыааем бесконечный проект
-        if project.goal is float('inf'):
+        if project.goal == float('inf'):
             self.label_deadline.setVisible(False)
             self.deadline.setVisible(False)
             self.label_max_streak.setVisible(False)
             self.max_streak.setVisible(False)
+            self.label_progress.setVisible(False)
+            self.progress.setVisible(False)
 
         # Основная информация
         self.status.setText(project.status)
