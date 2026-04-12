c474472b45941c42b920e65b4a2d5e5f83d6162d
nevskyforever <nevskyforever@mail.ru>
2026-04-12 17:30:28 +0400

    Обновление 3.4.1
    Теперь при активных стриках нельзя уменьшить цель на день, если она сегодня не выполнена, ее можно только увеличить.

--- engine.py
+++ engine.py
@@ -13,7 +13,7 @@ from docx import Document
 dev_mode = False
 
 # Версия приложения
-version = '3.4'
+version = '3.4.1'
 
 # Определяем систему
 SYSTEM = platform.system()  # 'Windows', 'Darwin' (macOS), 'Linux'
