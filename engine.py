5db82490f52f4483a076a52603d6d54138620108
nevskyforever <nevskyforever@mail.ru>
2026-04-20 20:08:21 +0400

    Обновление 3.6.3
    Исправлена логика глобального стрика - не учитывались завершенные проекты.

--- engine.py
+++ engine.py
@@ -13,7 +13,7 @@ from docx import Document
 dev_mode = False
 
 # Версия приложения
-version = '3.6.2'
+version = '3.6.3'
 
 # Определяем систему
 SYSTEM = platform.system()  # 'Windows', 'Darwin' (macOS), 'Linux'
