b28b59fc13bf060768fc9c506e43536e2e03f89b
nevskyforever <nevskyforever@mail.ru>
2026-05-20 23:39:45 +0400

    Обновление 3.7.1
    Исправление багов, мелкие улучшения игрового режима.

--- engine.py
+++ engine.py
@@ -11,10 +11,10 @@ from docx import Document
 import game
 
 # Режим разработчика
-dev_mode = True
+dev_mode = False
 
 # Версия приложения
-version = '3.7'
+version = '3.7.1'
 
 # Определяем систему
 SYSTEM = platform.system()  # 'Windows', 'Darwin' (macOS), 'Linux'
