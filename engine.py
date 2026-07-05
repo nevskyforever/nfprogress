cad01cf6beeed7d0fc62537616dd8e0bf7313b29
nevskyforever <nevskyforever@mail.ru>
2026-07-06 03:10:07 +0400

    Обновление 3.9
    В обновлении 3.9 игровой режим стал глубже. Теперь параметры персонажа отображаются в отдельной вкладке. Добавлены бафы и дебафы: временные и постоянные эффекты, которые могут усиливать или ослаблять персонажа. Появились новые зелья для увеличения получаемого опыта и монет. Также улучшен баланс цен, чтобы предметы и зелья были полезнее и доступнее. Раздел квестов уже появился в интерфейсе, но пока находится в разработке.

--- engine.py
+++ engine.py
@@ -13,7 +13,7 @@ from docx import Document
 dev_mode = False
 
 # Версия приложения
-version = '3.8'
+version = '3.9'
 
 # Определяем систему
 SYSTEM = platform.system()  # 'Windows', 'Darwin' (macOS), 'Linux'
