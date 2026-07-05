669bcda903ff02aa1d413d43eebecfc75d96c3cc
nevskyforever <nevskyforever@mail.ru>
2026-07-06 03:10:30 +0400

    Revert "Обновление 3.9"
    This reverts commit cad01cf6beeed7d0fc62537616dd8e0bf7313b29.

--- engine.py
+++ engine.py
@@ -13,7 +13,7 @@ from docx import Document
 dev_mode = False
 
 # Версия приложения
-version = '3.9'
+version = '3.8'
 
 # Определяем систему
 SYSTEM = platform.system()  # 'Windows', 'Darwin' (macOS), 'Linux'
