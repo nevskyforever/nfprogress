e551c64ba8900075c434f6363c7fdc9b7de39ffb
nevskyforever <nevskyforever@mail.ru>
2026-07-06 15:17:53 +0400

    Добавлено поле суммы длля досрочного погашения

--- UI_fiiles/bank.py
+++ UI_fiiles/bank.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'bankYjdoPt.ui'
+## Form generated from reading UI file 'bankCXvJXc.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.10.2
 ##
@@ -16,13 +16,13 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QLabel,
-    QPushButton, QSizePolicy, QWidget)
+    QLineEdit, QPushButton, QSizePolicy, QWidget)
 
 class Ui_Bamk(object):
     def setupUi(self, Bamk):
         if not Bamk.objectName():
             Bamk.setObjectName(u"Bamk")
-        Bamk.resize(364, 304)
+        Bamk.resize(364, 316)
         font = QFont()
         font.setFamilies([u"Arial"])
         Bamk.setFont(font)
@@ -38,11 +38,29 @@ class Ui_Bamk(object):
         self.widget.setObjectName(u"widget")
         self.gridLayout_2 = QGridLayout(self.widget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.make_deposit_btn = QPushButton(self.widget)
-        self.make_deposit_btn.setObjectName(u"make_deposit_btn")
+        self.make_a_loan_payment = QPushButton(self.widget)
+        self.make_a_loan_payment.setObjectName(u"make_a_loan_payment")
+
+        self.gridLayout_2.addWidget(self.make_a_loan_payment, 2, 0, 1, 1)
+
+        self.return_credit_btn = QPushButton(self.widget)
+        self.return_credit_btn.setObjectName(u"return_credit_btn")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.return_credit_btn.sizePolicy().hasHeightForWidth())
+        self.return_credit_btn.setSizePolicy(sizePolicy)
+
+        self.gridLayout_2.addWidget(self.return_credit_btn, 6, 0, 1, 1)
+
+        self.partial_loan_repayment = QPushButton(self.widget)
+        self.partial_loan_repayment.setObjectName(u"partial_loan_repayment")
+        self.partial_loan_repayment.setFlat(False)
+
+        self.gridLayout_2.addWidget(self.partial_loan_repayment, 4, 0, 1, 1)
+
+        self.make_deposit_btn = QPushButton(self.widget)
+        self.make_deposit_btn.setObjectName(u"make_deposit_btn")
         sizePolicy.setHeightForWidth(self.make_deposit_btn.sizePolicy().hasHeightForWidth())
         self.make_deposit_btn.setSizePolicy(sizePolicy)
 
@@ -60,7 +78,7 @@ class Ui_Bamk(object):
         sizePolicy.setHeightForWidth(self.withdraw_interest_from_a_deposit.sizePolicy().hasHeightForWidth())
         self.withdraw_interest_from_a_deposit.setSizePolicy(sizePolicy)
 
-        self.gridLayout_2.addWidget(self.withdraw_interest_from_a_deposit, 3, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.withdraw_interest_from_a_deposit, 4, 1, 1, 1)
 
         self.take_credit_btn = QPushButton(self.widget)
         self.take_credit_btn.setObjectName(u"take_credit_btn")
@@ -69,23 +87,10 @@ class Ui_Bamk(object):
 
         self.gridLayout_2.addWidget(self.take_credit_btn, 1, 0, 1, 1)
 
-        self.return_credit_btn = QPushButton(self.widget)
-        self.return_credit_btn.setObjectName(u"return_credit_btn")
-        sizePolicy.setHeightForWidth(self.return_credit_btn.sizePolicy().hasHeightForWidth())
-        self.return_credit_btn.setSizePolicy(sizePolicy)
+        self.loan_partial_repayment_amount = QLineEdit(self.widget)
+        self.loan_partial_repayment_amount.setObjectName(u"loan_partial_repayment_amount")
 
-        self.gridLayout_2.addWidget(self.return_credit_btn, 5, 0, 1, 1)
-
-        self.make_a_loan_payment = QPushButton(self.widget)
-        self.make_a_loan_payment.setObjectName(u"make_a_loan_payment")
-
-        self.gridLayout_2.addWidget(self.make_a_loan_payment, 2, 0, 1, 1)
-
-        self.partial_loan_repayment = QPushButton(self.widget)
-        self.partial_loan_repayment.setObjectName(u"partial_loan_repayment")
-        self.partial_loan_repayment.setFlat(False)
-
-        self.gridLayout_2.addWidget(self.partial_loan_repayment, 3, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.loan_partial_repayment_amount, 3, 0, 1, 1)
 
 
         self.gridLayout.addWidget(self.widget, 5, 0, 1, 3)
@@ -144,13 +149,14 @@ class Ui_Bamk(object):
     def retranslateUi(self, Bamk):
         Bamk.setWindowTitle(QCoreApplication.translate("Bamk", u"\u0411\u0430\u043d\u043a", None))
         self.deposit_status.setText(QCoreApplication.translate("Bamk", u"\u0412 \u0431\u0430\u043d\u043a\u0435 \u043d\u0435\u0442 \u0432\u043a\u043b\u0430\u0434\u0430", None))
+        self.make_a_loan_payment.setText(QCoreApplication.translate("Bamk", u"\u0412\u043d\u0435\u0441\u0442\u0438 \u043f\u043b\u0430\u0442\u0435\u0436", None))
+        self.return_credit_btn.setText(QCoreApplication.translate("Bamk", u"\u041f\u043e\u0433\u0430\u0441\u0438\u0442\u044c \u043a\u0440\u0435\u0434\u0438\u0442", None))
+        self.partial_loan_repayment.setText(QCoreApplication.translate("Bamk", u"\u0427\u0430\u0441\u0442\u0438\u0447\u043d\u043e\u0435 \u043f\u043e\u0433\u0430\u0448\u0435\u043d\u0438\u0435", None))
         self.make_deposit_btn.setText(QCoreApplication.translate("Bamk", u"\u0412\u043d\u0435\u0441\u0442\u0438 \u0432\u043a\u043b\u0430\u0434", None))
         self.return_deposit_btn.setText(QCoreApplication.translate("Bamk", u"\u0421\u043d\u044f\u0442\u044c \u0432\u043a\u043b\u0430\u0434", None))
         self.withdraw_interest_from_a_deposit.setText(QCoreApplication.translate("Bamk", u"\u0421\u043d\u044f\u0442\u044c \u043f\u0440\u043e\u0446\u0435\u043d\u0442\u044b", None))
         self.take_credit_btn.setText(QCoreApplication.translate("Bamk", u"\u0412\u0437\u044f\u0442\u044c \u043a\u0440\u0435\u0434\u0438\u0442", None))
-        self.return_credit_btn.setText(QCoreApplication.translate("Bamk", u"\u041f\u043e\u0433\u0430\u0441\u0438\u0442\u044c \u043a\u0440\u0435\u0434\u0438\u0442", None))
-        self.make_a_loan_payment.setText(QCoreApplication.translate("Bamk", u"\u0412\u043d\u0435\u0441\u0442\u0438 \u043f\u043b\u0430\u0442\u0435\u0436", None))
-        self.partial_loan_repayment.setText(QCoreApplication.translate("Bamk", u"\u0427\u0430\u0441\u0442\u0438\u0447\u043d\u043e\u0435 \u043f\u043e\u0433\u0430\u0448\u0435\u043d\u0438\u0435", None))
+        self.loan_partial_repayment_amount.setPlaceholderText(QCoreApplication.translate("Bamk", u"\u0421\u0443\u043c\u043c\u0430 \u043a \u043f\u043e\u0433\u0430\u0448\u0435\u043d\u0438\u044e (100.0)", None))
         self.return_credit_date.setText(QCoreApplication.translate("Bamk", u"\u0414\u0430\u0442\u0430 \u0432\u043e\u0437\u0432\u0440\u0430\u0442\u0430", None))
         self.return_deposit_date.setText(QCoreApplication.translate("Bamk", u"\u0414\u0430\u0442\u0430 \u0441\u043d\u044f\u0442\u0438\u044f", None))
         self.credit_total_sum.setText(QCoreApplication.translate("Bamk", u"\u0421\u0443\u043c\u043c\u0430 \u043a \u0432\u043e\u0437\u0432\u0440\u0430\u0442\u0443", None))
