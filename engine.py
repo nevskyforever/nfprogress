a2d15cfd0ac90c084051bd3952f1381482536799
nevskyforever <nevskyforever@mail.ru>
2026-07-08 23:06:05 +0400

    Обновление 3.11
    Теперь приложение проверяет наличие обновления в сети и предлагает загрузить его напрямую с сайта. Обновление можно отложить или пропустить. Пока необходимо устанавливать вручную, но теперь не нужно заходить на сайт или отследивать обновления в канале.

--- engine.py
+++ engine.py
@@ -10,10 +10,10 @@ from collections import defaultdict
 from docx import Document
 
 # Режим разработчика
-dev_mode = True
+dev_mode = False
 
 # Версия приложения
-version = '3.10.1'
+version = '3.11'
 
 # Определяем систему
 
