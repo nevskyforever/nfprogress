1f8ce3494ffa01d65b71f21142409dfaab8b1130
nevskyforever <nevskyforever@mail.ru>
2026-07-09 01:08:37 +0400

    Обновление 4.0
    Квесты  Добавлен новый раздел «Квесты».
    Теперь в игровом режиме доступны задания с разными условиями: развитие персонажа, написание текстов, стрики, глобальный стрик, банк, инвентарь и коллекция наград. Квесты делятся на доступные, активные и завершённые. Их можно брать, отслеживать прогресс и получать награды после выполнения условий.
    За выполнение квестов начисляются монеты, опыт, предметы-награды и временные бафы к коэффициентам монет и опыта. Чем сложнее квест, тем сильнее и дольше действует баф.  На данный момент в игровом режиме доступно 35 квестов, которые открываются вместе с повышением уровня вашего персонажа. Их количество будет пополняться.

--- engine.py
+++ engine.py
@@ -10,10 +10,10 @@ from collections import defaultdict
 from docx import Document
 
 # Режим разработчика
-dev_mode = True
+dev_mode = False
 
 # Версия приложения
-version = '3.11'
+version = '4.0'
 
 # Определяем систему
 
