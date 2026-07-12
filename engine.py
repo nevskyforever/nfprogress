f7ab5db50df6437b40d51f7403db074a29235958
nevskyforever <nevskyforever@mail.ru>
2026-07-12 20:22:18 +0400

    Обновление 4.3
    Умения
    
    В игровой режим добавлены Умения - навыки, кторые вы можете прокачивать с каждым уровнем. Каждый уровень вам дарит 2 балла умения, которые можно потратить на Дозодность (повыышает доход), "Производительность" (повышает кол-во опыта), "Выносливость" (повышает кол-во здоровтя, восстанавливаемого в час).
    
    Прогрессивная шкала здоровья - за каждые 5 уровней вы получаее +10 к максимальному значению здоровья.

--- engine.py
+++ engine.py
@@ -10,10 +10,10 @@ from collections import defaultdict
 from docx import Document
 
 # Режим разработчика
-dev_mode = True
+dev_mode = False
 
 # Версия приложения
-version = '4.2.3'
+version = '4.3'
 
 # Определяем систему
 
