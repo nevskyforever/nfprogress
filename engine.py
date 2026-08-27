d91dab66c9248448564e231faa123bd8cd15c7ef
nevskyforever <nevskyforever@mail.ru>
2026-08-28 01:15:22 +0400

    Обновление 5.1
    Интерфейс продолжает дорабатываться - в игровом режиме появился плавающий виджет с. информацией о персонаже, некоторые разделы перенесены в верхнее меню. В магазине появилась корзина - нажмите shift+купить - и соберите свою корзину.  У проектов появилась возможность добавлять обложку - она будет отображаться в списке проектов и в картинке шеринга прогресса.  Картинка шеринга прогресса изменилась - теперь она аналогична карточке проекта в списке.

--- engine.py
+++ engine.py
@@ -21,7 +21,7 @@ MAX_PROJECT_COVER_IMAGE_LENGTH = 5_000_000
 # Режим разработчика.
 dev_mode = "__compiled__" not in globals()
 
-# Версия приложения.
+# Версия приложения
 version = '5.1'
 
 # Определяем систему
